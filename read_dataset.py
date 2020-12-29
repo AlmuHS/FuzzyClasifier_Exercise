5182495b995bc8cb2dff94e9d879516f1f90409b
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-30 00:44:45 +0100

    improved function to allow select range from overleaped range series

--- read_dataset.py
+++ read_dataset.py
@@ -34,20 +34,46 @@ def set_tags(df: pd.DataFrame):
                 
         return tags
 
+def check_range(value: float, min: float, max: float):
+        return ((value >= min) and (value <= max))
+
+
 def select_tag_for_key(value: float, tag_range: list):
         tag_range_high = tag_range["High"]
         tag_range_medium = tag_range["Medium"]
         tag_range_low = tag_range["Low"]
 
+        tag_list = []
         tag = ""
         
-        if value >= tag_range_high[0] and value <= tag_range_high[1]:
-                tag = "High"
-        elif value >= tag_range_medium[0] and value <= tag_range_medium[1]:
-                tag = "Medium"
-        elif value >= tag_range_low[0] and value <= tag_range_low[1]:
-                tag = "Low" 
+        if check_range(value, tag_range_high[0], tag_range_high[1]):
+                tag_list.append("High")
+        
+        if check_range(value, tag_range_medium[0], tag_range_medium[1]):
+                tag_list.append("Medium")
+        
+        if check_range(value, tag_range_low[0], tag_range_low[1]):
+                tag_list.append("Low")
+        
+        if len(tag_list) == 1:
+                tag = tag_list[0]
+        else:
+                min_center_distance = 100000000000000000000000.0
 
+                tag = tag_list[0]
+
+                for tag_ in tag_list:
+                        t_range = tag_range[tag_]
+                        t_range_min = t_range[0]
+                        t_range_max = t_range[1]
+                
+                        center_value = (t_range_max-t_range_min)/2
+                        center_distance = value - center_value
+                
+                        if(center_distance < min_center_distance):
+                                min_center_distance = center_distance
+                                tag = tag_
+                
         return tag
 
 
@@ -90,17 +116,17 @@ def get_training_rules(df: pd.DataFrame, tags_ranges: dict, types: list):
 def classify_dataset(fuzzy_df: pd.DataFrame, rules_df: pd.DataFrame):
         merge_in  = pd.merge(fuzzy_df, rules_df, how = 'outer', on = list(fuzzy_df.columns))
         
-#        for i, row in fuzzy_df.iterrows():
-#                match_list = []
-#        
-#                for j, rule in rules_df.iterrows():                
-#                        if tuple(row) == tuple(rule[:-1]):
-#                                match_list.append(rule)
-#                                #print(f"{tuple(row)}\n{tuple(rule)}\n\n")
-#                
-#                print(f"row {tuple(row)}: {len(match_list)} matches")
-#                if len(match_list) > 1:
-#                        print(match_list)
+        for i, row in fuzzy_df.iterrows():
+                match_list = []
+        
+                for j, rule in rules_df.iterrows():                
+                        if tuple(row) == tuple(rule[:-1]):
+                                match_list.append(rule)
+                                #print(f"{tuple(row)}\n{tuple(rule)}\n\n")
+                
+                print(f"row {tuple(row)}: {len(match_list)} matches")
+                if len(match_list) > 1:
+                        print(match_list)
         
         return merge_in
 
@@ -118,9 +144,8 @@ types = range(1,8)
 rules_training = get_training_rules(df, tags_ranges, types)
 print(rules_training)
 
-#print(df.columns)
 fuzzy_df = fuzzify_dataset(df, tags_ranges)
-#print(fuzzy_df)
+print(fuzzy_df)
 
-#classified_df = classify_dataset(fuzzy_df, rules_training)
-#print(classified_df)
+classified_df = classify_dataset(fuzzy_df, rules_training)
+print(classified_df)
