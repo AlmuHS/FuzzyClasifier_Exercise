faa9bd46bb0593be0dfa43ae0e14f6050b6a21ed
Almudena Garcia <liberamenso10000@gmail.com>
2021-01-08 15:45:15 +0100

    classifier: verify match without loss owning degree column

--- classifier.py
+++ classifier.py
@@ -7,15 +7,6 @@ class Classifier:
         self.rules_df = rules_df
 
     def select_type(self, row: tuple):
-        # row_df = pd.DataFrame(row).drop(['Type'], axis=0)
-        # rules_filtered = self.rules_df.drop(['Owning Degree', 'Type'], axis=1)
-
-        # try:
-        #     match = self.rules_df[rules_filtered == row_df]
-        # except:
-        #     row['Type'] = -1
-        #     row['Owning Degree'] = 0
-
         for i, rule in self.rules_df.iterrows():
             if tuple(rule[:-2]) == tuple(row[:-1]):
                 return rule
@@ -32,8 +23,9 @@ class Classifier:
         return self.classified_df
 
     def verify_classification(self):
-        matched = self.fuzzy_df[self.fuzzy_df.isin(
-            self.classified_df[:-1])].dropna()
+        keys = self.classified_df.columns[:-1]
+        matched = self.classified_df.loc[(self.classified_df[keys]
+                                          == self.fuzzy_df[keys]).all(axis="columns")]
 
         TP_value = len(matched)
 
